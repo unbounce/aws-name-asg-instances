e3989bf4c2fa58db5cb063e7398100c439143bbb
Scott Brown <scott.brown@unbounce.com>
2018-03-19 16:09:35 -0700

    Formatting

--- main.go
+++ main.go
@@ -1,190 +1,189 @@
 package main
 
 import (
-  "errors"
-  "fmt"
-  "github.com/aws/aws-sdk-go-v2/aws"
-  "github.com/aws/aws-sdk-go-v2/aws/external"
-  "github.com/aws/aws-sdk-go-v2/service/ec2"
-  "github.com/aws/aws-lambda-go/lambda"
-  "github.com/aws/aws-lambda-go/events"
-  "context"
-  "encoding/json"
-  "strings"
+	"context"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/aws/external"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"strings"
 )
 
 const (
-  MAX_TAG_LENGTH int = 255
-  TAG_PROJECT string = "project"
-  TAG_ENV string = "environment"
+	MAX_TAG_LENGTH int    = 255
+	TAG_PROJECT    string = "project"
+	TAG_ENV        string = "environment"
 )
 
 var (
-  NameAlreadySetError = errors.New("Name already set")
-  TagNotFoundError = errors.New("Tag not found")
-  InstanceNotFoundError = errors.New("Instance not found")
-  MultipleInstancesError = errors.New("Multiple instances found")
+	NameAlreadySetError    = errors.New("Name already set")
+	TagNotFoundError       = errors.New("Tag not found")
+	InstanceNotFoundError  = errors.New("Instance not found")
+	MultipleInstancesError = errors.New("Multiple instances found")
 )
 
 type AutoScalingEvent struct {
-  EC2InstanceId string `json:"EC2InstanceId"`
+	EC2InstanceId string `json:"EC2InstanceId"`
 }
 
 func Handler(ctx context.Context, event events.CloudWatchEvent) {
-  var data AutoScalingEvent
-  err := json.Unmarshal(event.Detail, &data)
-  if err != nil {
-    panic(err.Error())
-  }
+	var data AutoScalingEvent
+	err := json.Unmarshal(event.Detail, &data)
+	if err != nil {
+		panic(err.Error())
+	}
 
-  cfg, err := external.LoadDefaultAWSConfig()
-  if err != nil {
-    panic(err.Error())
-  }
+	cfg, err := external.LoadDefaultAWSConfig()
+	if err != nil {
+		panic(err.Error())
+	}
 
-  verifyInstanceExists(cfg, data.EC2InstanceId)
-  tagList := getTags(cfg, data.EC2InstanceId)
+	verifyInstanceExists(cfg, data.EC2InstanceId)
+	tagList := getTags(cfg, data.EC2InstanceId)
 
-  if nameTagSet(tagList) {
-    fmt.Printf("Name tag already set on instance: %s\n", data.EC2InstanceId)
-    return
-  }
+	if nameTagSet(tagList) {
+		fmt.Printf("Name tag already set on instance: %s\n", data.EC2InstanceId)
+		return
+	}
 
-  projectTag, err := getTagValue(tagList, TAG_PROJECT)
-  if err != nil {
-    fmt.Printf("Missing required tag: %s\n", TAG_PROJECT)
-    return
-  }
-  envTag, err := getTagValue(tagList, TAG_ENV)
-  if err != nil {
-    fmt.Printf("Missing required tag: %s\n", TAG_ENV)
-    return
-  }
+	projectTag, err := getTagValue(tagList, TAG_PROJECT)
+	if err != nil {
+		fmt.Printf("Missing required tag: %s\n", TAG_PROJECT)
+		return
+	}
+	envTag, err := getTagValue(tagList, TAG_ENV)
+	if err != nil {
+		fmt.Printf("Missing required tag: %s\n", TAG_ENV)
+		return
+	}
 
-  name := buildName(projectTag, envTag, data.EC2InstanceId)
-  nameInstance(cfg, data.EC2InstanceId, name)
+	name := buildName(projectTag, envTag, data.EC2InstanceId)
+	nameInstance(cfg, data.EC2InstanceId, name)
 }
 
 func main() {
-  lambda.Start(Handler)
+	lambda.Start(Handler)
 }
 
 func verifyInstanceExists(cfg aws.Config, instanceId string) error {
-  svc := ec2.New(cfg)
+	svc := ec2.New(cfg)
 
-  includeAll := true
-  params := ec2.DescribeInstanceStatusInput{
-    InstanceIds: []string{ instanceId },
-    IncludeAllInstances: &includeAll,
-  }
+	includeAll := true
+	params := ec2.DescribeInstanceStatusInput{
+		InstanceIds:         []string{instanceId},
+		IncludeAllInstances: &includeAll,
+	}
 
-  req := svc.DescribeInstanceStatusRequest(&params)
-  result, err := req.Send()
-  if err != nil {
-    panic(err.Error())
-  }
+	req := svc.DescribeInstanceStatusRequest(&params)
+	result, err := req.Send()
+	if err != nil {
+		panic(err.Error())
+	}
 
-  if len(result.InstanceStatuses) < 1 {
-    fmt.Printf("Instance not found: %s\n", instanceId)
-    return InstanceNotFoundError
-  }
+	if len(result.InstanceStatuses) < 1 {
+		fmt.Printf("Instance not found: %s\n", instanceId)
+		return InstanceNotFoundError
+	}
 
-  if len(result.InstanceStatuses) > 1 {
-    fmt.Printf("1+ instances found for instance %s.  Aborting to prevent damage.\n", instanceId)
-    return MultipleInstancesError
-  }
+	if len(result.InstanceStatuses) > 1 {
+		fmt.Printf("1+ instances found for instance %s.  Aborting to prevent damage.\n", instanceId)
+		return MultipleInstancesError
+	}
 
-  return nil
+	return nil
 }
 
 func getTags(cfg aws.Config, instanceId string) []ec2.TagDescription {
-  svc := ec2.New(cfg)
+	svc := ec2.New(cfg)
 
-  filterName := "resource-id"
-  params := ec2.DescribeTagsInput{
-    Filters: []ec2.Filter {
-      ec2.Filter{
-        Name: &filterName,
-        Values: []string{ instanceId },
-      },
-    },
-  }
+	filterName := "resource-id"
+	params := ec2.DescribeTagsInput{
+		Filters: []ec2.Filter{
+			ec2.Filter{
+				Name:   &filterName,
+				Values: []string{instanceId},
+			},
+		},
+	}
 
-  req := svc.DescribeTagsRequest(&params)
-  result, err := req.Send()
-  if err != nil {
-    panic(err.Error())
-  }
+	req := svc.DescribeTagsRequest(&params)
+	result, err := req.Send()
+	if err != nil {
+		panic(err.Error())
+	}
 
-  return result.Tags
+	return result.Tags
 }
 
 func nameTagSet(tagList []ec2.TagDescription) bool {
-  tagFound := false
-  for _, i := range tagList {
-    if *i.Key == "Name" && *i.Value != "" {
-      tagFound = true
-    }
-  }
+	tagFound := false
+	for _, i := range tagList {
+		if *i.Key == "Name" && *i.Value != "" {
+			tagFound = true
+		}
+	}
 
-  return tagFound
+	return tagFound
 }
 
 func getTagValue(tagList []ec2.TagDescription, key string) (string, error) {
-  var val string
-  for _, i := range tagList {
-    if *i.Key == key && *i.Value != "" {
-      val = *i.Value
-    }
-  }
+	var val string
+	for _, i := range tagList {
+		if *i.Key == key && *i.Value != "" {
+			val = *i.Value
+		}
+	}
 
-  if val == "" {
-    return "", TagNotFoundError
-  }
+	if val == "" {
+		return "", TagNotFoundError
+	}
 
-  return val, nil
+	return val, nil
 }
 
 func buildName(project string, env string, instanceId string) string {
-  if project == "" || env == "" || instanceId == "" {
-    return ""
-  }
-  var id string
-  id_parts := strings.Split(instanceId, "-")
-  if len(id_parts) == 2 {
-    id = id_parts[1]
-  } else {
-    return ""
-  }
-  name := fmt.Sprintf("%s-%s-%s", project, env, id)
-  if len(name) > MAX_TAG_LENGTH {
-    name = name[0:MAX_TAG_LENGTH]
-  }
-  return name
+	if project == "" || env == "" || instanceId == "" {
+		return ""
+	}
+	var id string
+	id_parts := strings.Split(instanceId, "-")
+	if len(id_parts) == 2 {
+		id = id_parts[1]
+	} else {
+		return ""
+	}
+	name := fmt.Sprintf("%s-%s-%s", project, env, id)
+	if len(name) > MAX_TAG_LENGTH {
+		name = name[0:MAX_TAG_LENGTH]
+	}
+	return name
 }
 
 func nameInstance(cfg aws.Config, instanceId string, name string) {
-  svc := ec2.New(cfg)
+	svc := ec2.New(cfg)
 
-  tagName := "Name"
+	tagName := "Name"
 
-  params := ec2.CreateTagsInput{
-    Resources: []string{ instanceId },
-    Tags: []ec2.Tag{
-      ec2.Tag{
-        Key: &tagName,
-        Value: &name,
-      },
-    },
-  }
+	params := ec2.CreateTagsInput{
+		Resources: []string{instanceId},
+		Tags: []ec2.Tag{
+			ec2.Tag{
+				Key:   &tagName,
+				Value: &name,
+			},
+		},
+	}
 
-  req := svc.CreateTagsRequest(&params)
-  _, err := req.Send()
-  if err != nil {
-    // TODO do more error checking here
-    panic(err.Error())
-  }
+	req := svc.CreateTagsRequest(&params)
+	_, err := req.Send()
+	if err != nil {
+		// TODO do more error checking here
+		panic(err.Error())
+	}
 
-  fmt.Printf("SUCCESS: Tagging %s with name %s", instanceId, name)
+	fmt.Printf("SUCCESS: Tagging %s with name %s", instanceId, name)
 }
-
--- main_test.go
+++ main_test.go
@@ -1,92 +1,91 @@
 package main
 
 import (
-  "testing"
-  "github.com/aws/aws-sdk-go-v2/service/ec2"
+	"github.com/aws/aws-sdk-go-v2/service/ec2"
+	"testing"
 )
 
 func TestNameTagSet(t *testing.T) {
-  cases := []struct{
-    Name string
-    keyName string
-    keyVal string
-    expected bool
-  }{
-    {"case1", "NotName", "foo", false},
-    {"case2", "Name", "foo", true},
-    {"case3", "Name", "", false},
-  }
+	cases := []struct {
+		Name     string
+		keyName  string
+		keyVal   string
+		expected bool
+	}{
+		{"case1", "NotName", "foo", false},
+		{"case2", "Name", "foo", true},
+		{"case3", "Name", "", false},
+	}
 
-  for _, tc := range cases {
-    tagList := []ec2.TagDescription{
-      ec2.TagDescription{
-        Key: &tc.keyName,
-        Value: &tc.keyVal,
-      },
-    }
+	for _, tc := range cases {
+		tagList := []ec2.TagDescription{
+			ec2.TagDescription{
+				Key:   &tc.keyName,
+				Value: &tc.keyVal,
+			},
+		}
 
-    actual := nameTagSet(tagList)
-    if actual != tc.expected {
-      t.Errorf("[%s] Expected %v but got %v", tc.Name, tc.expected, actual)
-    }
-  } 
+		actual := nameTagSet(tagList)
+		if actual != tc.expected {
+			t.Errorf("[%s] Expected %v but got %v", tc.Name, tc.expected, actual)
+		}
+	}
 }
 
 func TestGetTagValue(t *testing.T) {
-  cases := []struct{
-    Name string
-    keyName string
-    keyVal string
-    testKey string
-    resultVal string
-    errorExpected bool
-  }{
-    {"case1", "foo", "bar", "foo", "bar", false},
-    {"case2", "foo", "", "foo", "", true},
-    {"case3", "baz", "bar", "foo", "", true},
-  }
+	cases := []struct {
+		Name          string
+		keyName       string
+		keyVal        string
+		testKey       string
+		resultVal     string
+		errorExpected bool
+	}{
+		{"case1", "foo", "bar", "foo", "bar", false},
+		{"case2", "foo", "", "foo", "", true},
+		{"case3", "baz", "bar", "foo", "", true},
+	}
 
-  for _, tc := range cases {
-    tagList := []ec2.TagDescription{
-      ec2.TagDescription{
-        Key: &tc.keyName,
-        Value: &tc.keyVal,
-      },
-    }
+	for _, tc := range cases {
+		tagList := []ec2.TagDescription{
+			ec2.TagDescription{
+				Key:   &tc.keyName,
+				Value: &tc.keyVal,
+			},
+		}
 
-    val, err := getTagValue(tagList, tc.testKey)
-    if err != nil && !tc.errorExpected {
-      t.Errorf("[%s]: Expected %v but got error %v", tc.Name, tc.resultVal, err)
-    }
+		val, err := getTagValue(tagList, tc.testKey)
+		if err != nil && !tc.errorExpected {
+			t.Errorf("[%s]: Expected %v but got error %v", tc.Name, tc.resultVal, err)
+		}
 
-    if val != tc.resultVal {
-      t.Errorf("[%s]: Expected %v but got %v", tc.Name, tc.resultVal, val)
-    }
-  }
+		if val != tc.resultVal {
+			t.Errorf("[%s]: Expected %v but got %v", tc.Name, tc.resultVal, val)
+		}
+	}
 }
 
 func TestBuildName(t *testing.T) {
-  cases := []struct{
-    Name string
-    project string
-    env string
-    instanceId string
-    expected string
-  }{
-    {"case1", "foo", "bar", "i-0123", "foo-bar-0123"},
-    {"case2", "foo", "bar", "i-01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789", "foo-bar-0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456"}, // max length of tag value
-    {"case3", "", "bar", "i-0123", ""},
-    {"case4", "foo", "", "i-0123", ""},
-    {"case5", "foo", "bar", "0123", ""},
-    {"case6", "foo", "bar", "", ""},
-  }
- 
-  for _, tc := range cases {
-    actual := buildName(tc.project, tc.env, tc.instanceId)
+	cases := []struct {
+		Name       string
+		project    string
+		env        string
+		instanceId string
+		expected   string
+	}{
+		{"case1", "foo", "bar", "i-0123", "foo-bar-0123"},
+		{"case2", "foo", "bar", "i-01234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789", "foo-bar-0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456"}, // max length of tag value
+		{"case3", "", "bar", "i-0123", ""},
+		{"case4", "foo", "", "i-0123", ""},
+		{"case5", "foo", "bar", "0123", ""},
+		{"case6", "foo", "bar", "", ""},
+	}
 
-    if actual != tc.expected {
-      t.Errorf("[%s] Expected %v but got %v", tc.Name, tc.expected, actual)
-    }
-  }
+	for _, tc := range cases {
+		actual := buildName(tc.project, tc.env, tc.instanceId)
+
+		if actual != tc.expected {
+			t.Errorf("[%s] Expected %v but got %v", tc.Name, tc.expected, actual)
+		}
+	}
 }
-
